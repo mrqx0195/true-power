e14799de2fb1f62abc3c8c42ad18b6601248882d
墨染琴弦 <2317249571@qq.com>
2026-07-18 13:51:11 +0800

    v1.2.1-hotfix1

--- src/main/java/net/mrqx/truepower/compat/kubejs/TruePowerKubeJS.java
+++ src/main/java/net/mrqx/truepower/compat/kubejs/TruePowerKubeJS.java
@@ -2,11 +2,9 @@ package net.mrqx.truepower.compat.kubejs;
 
 import dev.latvian.mods.kubejs.KubeJSPaths;
 import dev.latvian.mods.kubejs.server.GeneratedServerResourcePack;
-import dev.latvian.mods.kubejs.server.ServerScriptManager;
 import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
-import net.mrqx.truepower.mixin.AccessorReloadableResourceManager;
 
 import java.io.File;
 import java.util.List;
@@ -14,21 +12,21 @@ import java.util.Objects;
 
 public class TruePowerKubeJS {
     public static void onCustomResourceLoad(List<PackResources> packs, ReloadableResourceManager resourceManager) {
-        if (resourceManager instanceof AccessorReloadableResourceManager accessor) {
-            ServerScriptManager oldInstance = ServerScriptManager.instance;
-            try {
-                ServerScriptManager.instance = new ServerScriptManager(null);
-                accessor.setResources(ServerScriptManager.instance.wrapResourceManager(accessor.getResources()));
-            } catch (Exception e) {
-                packs.add(new GeneratedServerResourcePack());
-                for (File file : Objects.requireNonNull(KubeJSPaths.DATA.toFile().listFiles())) {
-                    if (file.isFile() && file.getName().endsWith(".zip")) {
-                        packs.add(new FilePackResources(file.getName(), file, false));
-                    }
-                }
-            } finally {
-                ServerScriptManager.instance = oldInstance;
+//        if (resourceManager instanceof AccessorReloadableResourceManager accessor) {
+//            ServerScriptManager oldInstance = ServerScriptManager.instance;
+//            try {
+//                ServerScriptManager.instance = new ServerScriptManager(null);
+//                accessor.setResources(ServerScriptManager.instance.wrapResourceManager(accessor.getResources()));
+//            } catch (Exception e) {
+        packs.add(new GeneratedServerResourcePack());
+        for (File file : Objects.requireNonNull(KubeJSPaths.DATA.toFile().listFiles())) {
+            if (file.isFile() && file.getName().endsWith(".zip")) {
+                packs.add(new FilePackResources(file.getName(), file, false));
             }
         }
+//            } finally {
+//                ServerScriptManager.instance = oldInstance;
+//            }
+//        }
     }
 }
