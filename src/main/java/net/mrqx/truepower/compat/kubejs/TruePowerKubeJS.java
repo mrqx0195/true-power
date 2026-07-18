26a66aecee9a58ce8f72ac3661f2c4d4bfe8c5a1
墨染琴弦 <2317249571@qq.com>
2026-07-18 13:52:24 +0800

    v1.2.1-hotfix1

--- src/main/java/net/mrqx/truepower/compat/kubejs/TruePowerKubeJS.java
+++ src/main/java/net/mrqx/truepower/compat/kubejs/TruePowerKubeJS.java
@@ -2,29 +2,27 @@ package net.mrqx.truepower.compat.kubejs;
 
 import dev.latvian.mods.kubejs.KubeJSPaths;
 import dev.latvian.mods.kubejs.script.data.KubeFileResourcePack;
-import dev.latvian.mods.kubejs.server.ServerScriptManager;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
-import net.mrqx.truepower.mixin.AccessorServerScriptManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TruePowerKubeJS {
     public static void onCustomResourceLoad(List<PackResources> packs) {
-        ServerScriptManager staticInstance = AccessorServerScriptManager.getStaticInstance();
-        try {
-            List<PackResources> packResources = ServerScriptManager.createPackResources(packs);
-            packs.clear();
-            packs.addAll(packResources);
-        } catch (Throwable e) {
-            List<PackResources> filePacks = new ArrayList<>();
-            KubeFileResourcePack.scanAndLoad(KubeJSPaths.DATA, filePacks);
-            filePacks.sort((p1, p2) -> p1.packId().compareToIgnoreCase(p2.packId()));
-            filePacks.add(new KubeFileResourcePack(PackType.SERVER_DATA));
-            packs.addAll(filePacks);
-        } finally {
-            AccessorServerScriptManager.setStaticInstance(staticInstance);
-        }
+//        ServerScriptManager staticInstance = AccessorServerScriptManager.getStaticInstance();
+//        try {
+//            List<PackResources> packResources = ServerScriptManager.createPackResources(packs);
+//            packs.clear();
+//            packs.addAll(packResources);
+//        } catch (Exception e) {
+        List<PackResources> filePacks = new ArrayList<>();
+        KubeFileResourcePack.scanAndLoad(KubeJSPaths.DATA, filePacks);
+        filePacks.sort((p1, p2) -> p1.packId().compareToIgnoreCase(p2.packId()));
+        filePacks.add(new KubeFileResourcePack(PackType.SERVER_DATA));
+        packs.addAll(filePacks);
+//        } finally {
+//            AccessorServerScriptManager.setStaticInstance(staticInstance);
+//        }
     }
 }
